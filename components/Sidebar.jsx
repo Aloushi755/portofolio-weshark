ddc746380db8eb2cbc0df99d3315f503ae13d43e
Aloushi755 <alimoussa755@hotmail.com>
2024-11-01 11:12:37 +0100

    Smaller sidebar on mobile

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Sidebar = () => (
-    <aside className="flex w-12 h-screen bg-green_medium fixed z-30">
+    <aside className="flex w-8 md:w-12 h-screen bg-green_medium fixed z-30">
         <div className="flex flex-col pt-4 px-2">
             <a href="#"><img loading='lazy' src="/logo.png" alt="Weshark" /></a>
         </div>
