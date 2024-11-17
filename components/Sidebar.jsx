1ef263a9dc4403b93785bd5bd494332f7bca62cc
Aloushi755 <alimoussa755@hotmail.com>
2024-11-17 11:36:12 +0100

    sidebar smaller on mobile - test

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 
 const Sidebar = () => (
-    <aside className="flex w-10 md:w-12 h-screen bg-green_medium fixed z-30">
+    <aside className="flex w-8 md:w-12 h-screen bg-green_medium fixed z-30">
         <div className="flex flex-col pt-4 px-2">
             <a href="#">
                 <Image
