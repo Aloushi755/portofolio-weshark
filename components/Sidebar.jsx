a2d43ebaa4b206f12e9d17ba29ce3053863b4cb7
Aloushi755 <alimoussa755@hotmail.com>
2024-11-17 11:38:16 +0100

    sidebar adjustments

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Sidebar = () => (
     <aside className="flex w-8 md:w-12 h-screen bg-green_medium fixed z-30">
-        <div className="flex flex-col pt-4 px-2">
+        <div className="flex flex-col pt-4 px-1">
             <a href="#">
                 <Image
                     src="/logo.png"
