39f8db32be330086a5a455860d0bda22ef123fcc
Aloushi755 <alimoussa755@hotmail.com>
2025-02-21 17:46:51 +0100

    Made some adjustments

--- frontend/features/homepage/components/Hero.jsx
+++ frontend/features/homepage/components/Hero.jsx
@@ -8,7 +8,7 @@ export function Hero() {
         <section className='w-full bg-green_dark'>
             <div className="flex flex-col-reverse lg:flex-row justify-bewteen items-center max-w-7xl py-16 sm:py-20 lg:py-28 mx-auto gap-6">
                 <div className='flex flex-col justify-center items-start w-full pl-12 md:pl-32 lg:pl-10 lg:ml-16 lg:w-1/2'>
-                    <h1 className='py-4 text-lg leading-6 sm:text-2xl md:text-3xl lg:text-[2.5rem] lg:leading-[2.7rem] text-white uppercase'>
+                    <h1 className='py-4 text-xl leading-6 sm:text-2xl md:text-3xl lg:text-[2.5rem] lg:leading-[2.7rem] text-white uppercase'>
                         My name is Ali Moussa <br />
                         <span
                             className='text-green_light font-bold italic whitespace-nowrap'
--- frontend/features/homepage/components/PrincipleCard.jsx
+++ frontend/features/homepage/components/PrincipleCard.jsx
@@ -4,15 +4,15 @@ import React from 'react';
 export function PrincipleCard({ number, title, description }) {
     return (
         <article className='flex w-full max-w-lg justify-center items-center mx-auto'>
-            <div className='flex justify-center items-center gap-4'>
-                <div className='pt- md:pt-12 text-small sm:text-mega text-green_light'>
+            <div className='flex justify-center items-center gap-2'>
+                <div className='pt-6 md:pt-12 text-small sm:text-mega text-green_light'>
                     {number}
                 </div>
                 <div className='flex flex-col justify-center items-start'>
                     <h4 className='text-lg md:text-2xl font-light text-white uppercase'>
-                        {title}<span className='text-lg sm:text-8xl text-green_lime'>.</span>
+                        {title}<span className='text-4xl sm:text-8xl text-green_lime'>.</span>
                     </h4>
-                    <p className='text-green_army text-[0.7rem] md:text-[0.9rem] leading-[1.2rem] pr-1 sm:leading-6 md:mb-0'>{description}</p>
+                    <p className='text-green_army text-[0.8rem] md:text-[0.9rem] leading-[1.2rem] pr-1 sm:leading-6 md:mb-0'>{description}</p>
                 </div>
             </div>
         </article>
