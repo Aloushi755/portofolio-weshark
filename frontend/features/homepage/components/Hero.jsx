2482067a563d3d8c485995393a4112fca080adda
Aloushi755 <alimoussa755@hotmail.com>
2025-03-03 22:13:56 +0100

    Added a link to the cta on the hero page

--- frontend/features/homepage/components/Hero.jsx
+++ frontend/features/homepage/components/Hero.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { Typewriter } from 'react-simple-typewriter';
+import Link from 'next/link';
 
 export function Hero() {
     return (
@@ -24,9 +25,9 @@ export function Hero() {
                             />
                         </span>
                     </h1>
-                    <a href='#' className='px-6 py-2 border-green_light bg-green_light border-2 rounded-full text-xs md:text-md uppercase text-green_dark hover:bg-transparent hover:text-green_light transition duration-400 ease-in-out'>
-                        Download CV
-                    </a>
+                    <Link href='https://www.linkedin.com/in/ali-el-haj-moussa-b25b98289/' className='px-6 py-2 border-green_light bg-green_light border-2 rounded-full text-xs md:text-md uppercase text-green_dark hover:bg-transparent hover:text-green_light transition duration-400 ease-in-out'>
+                        Find me on LinkedIn
+                    </Link>
                 </div>
                 <div className='flex flex-col md:flex-row justify-center items-center mx-auto md:pl-0'>
                     <Image
